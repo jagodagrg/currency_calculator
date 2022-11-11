6c672a89407455b2537ad093575911fd743c7a1e
Jagoda Giurg <jagoda.giurg@gmail.com>
2022-11-11 22:40:36 +0000

    Fix issues

--- currencies.py
+++ currencies.py
@@ -2,23 +2,7 @@ from flask import Flask, render_template, request
 import requests
 import csv
 
-
 app = Flask(__name__)
-
-response = requests.get(
-    "http://api.nbp.pl/api/exchangerates/tables/C?format=json")
-data = response.json()
-
-rates = (data[0])["rates"]
-
-with open('/home/jagoda/Desktop/Kodilla/Python/currencies/currency_calculator.csv', 'w', newline='') as csvfile:
-    currencywriter = csv.writer(csvfile, delimiter=';')
-    currencywriter.writerow(['currency'] + ['code'] + ['bid'] + ['ask'])
-    codes = []
-    for item in rates:
-        currencywriter.writerow([
-            item['currency']] + [item['code']] + [str(item['bid'])] + [str(item['ask'])])
-        codes.append(item['code'])
 
 
 @app.route("/calculator/", methods=['GET', 'POST'])
@@ -28,11 +12,24 @@ def calculate_exchange_rate():
     if request.method == 'POST':
         currency = request.form['currency']
         amount = request.form['amount']
-        for rate in rates:
-            if rate['code'] == currency:
-                result = round(float(amount) * float(rate['ask']), 2)
-    return render_template('currency_calculator.html', codes=codes, result=result, currency=currency)
+        for code, rate in exchange_rates.items():
+            if code == currency:
+                result = round(float(amount) * float(rate), 2)
+    return render_template('currency_calculator.html', currency=currency, codes=exchange_rates.keys(), result=result)
 
 
 if __name__ == '__main__':
+    response = requests.get(
+        "http://api.nbp.pl/api/exchangerates/tables/C?format=json")
+    data = response.json()
+    rates = (data[0])["rates"]
+    exchange_rates = {}
+    for rate in rates:
+        exchange_rates[rate['code']] = rate['ask']
+    with open('currencies.csv', 'w', newline='') as csvfile:
+        currencywriter = csv.writer(csvfile, delimiter=';')
+        currencywriter.writerow(['currency', 'code', 'bid', 'ask'])
+        for rate in rates:
+            currencywriter.writerow(
+                [rate['currency'], rate['code'], str(rate['bid']), str(rate['ask'])])
     app.run(debug=True)
