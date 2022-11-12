5a93713759a2b9b62d4a2e732447ce362bc667a7
Jagoda Giurg <jagoda.giurg@gmail.com>
2022-11-12 19:18:49 +0000

    Define new function

--- app.py
+++ app.py
@@ -5,24 +5,11 @@ import csv
 app = Flask(__name__)
 
 
-@app.route("/calculator/", methods=['GET', 'POST'])
-def calculate_exchange_rate():
-    currency = None
-    result = 0
-    if request.method == 'POST':
-        currency = request.form['currency']
-        amount = request.form['amount']
-        for code, rate in exchange_rates.items():
-            if code == currency:
-                result = round(float(amount) * float(rate), 2)
-    return render_template('currency_calculator.html', currency=currency, codes=exchange_rates.keys(), result=result)
-
-
-if __name__ == '__main__':
+def get_exchange_data():
     response = requests.get(
         "http://api.nbp.pl/api/exchangerates/tables/C?format=json")
     data = response.json()
-    rates = (data[0])["rates"]
+    rates = (data[0])['rates']
     exchange_rates = {}
     for rate in rates:
         exchange_rates[rate['code']] = rate['ask']
@@ -32,4 +19,20 @@ if __name__ == '__main__':
         for rate in rates:
             currencywriter.writerow(
                 [rate['currency'], rate['code'], str(rate['bid']), str(rate['ask'])])
+    return exchange_rates
+
+
+@app.route("/calculator/", methods=['GET', 'POST'])
+def calculate_exchange_rate():
+    currency = None
+    result = 0
+    if request.method == 'POST':
+        currency = request.form['currency']
+        amount = request.form['amount']
+        result = round(float(amount) * float(exchange_rates[currency]), 2)
+    return render_template('currency_calculator.html', currency=currency, codes=exchange_rates.keys(), result=result)
+
+
+if __name__ == '__main__':
+    exchange_rates = get_exchange_data()
     app.run(debug=True)
